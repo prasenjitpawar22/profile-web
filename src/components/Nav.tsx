3cfbe44994b72bb57c15e1fdb696b0cb85624ea2
Prasenjit Pawar <prasenjit.pawar@capgemini.com>
2023-04-08 16:54:13 +0530

    deploy

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,20 +1,47 @@
 import React, { useState } from 'react'
 import { motion, useScroll, useMotionValueEvent } from 'framer-motion'
 import { Montserrat } from 'next/font/google'
+import { AiOutlineMenu } from 'react-icons/ai'
+import { RxCross1 } from 'react-icons/rx'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
-const Nav = () => {
+enum Type {
+    about,
+    contact,
+    experience,
+    work
+}
+interface Props {
+    aboutRef: React.MutableRefObject<HTMLDivElement | null>
+    experienceRef?: React.MutableRefObject<HTMLDivElement | null>
+    workRef?: React.MutableRefObject<HTMLDivElement | null>
+    contactRef?: React.MutableRefObject<HTMLDivElement | null>
+}
+
+const Nav = (props: Props) => {
+
+    const { aboutRef, experienceRef, workRef, contactRef } = props
+
     const { scrollY } = useScroll()
 
     const [hidden, setHidden] = useState(false)
-    const list = ['Link one', 'Link two', 'Link three']
+    const [sideBarState, setSideBarState] = useState(false)
+
+    const list = [{ text: 'about', type: Type.about },
+    { text: 'experience', type: Type.experience },
+    { text: 'work', type: Type.work },
+    { text: 'contact', type: Type.contact }]
 
     const variants = {
         visible: { opacity: 1, y: 0 },
         hidden: { opacity: 0, y: -25 }
     };
 
+    const sidebarVariants = {
+        visible: { opacity: 1, x: 0 },
+        hidden: { opacity: 0, x: window.innerWidth + 20 }
+    }
 
     useMotionValueEvent(scrollY, "change", (latest) => {
         if (latest < scrollY.getPrevious()) {
@@ -24,21 +51,57 @@ const Nav = () => {
         }
     })
 
+    const handleNavClick = (type: Type) => {
+        if (type === Type.about) aboutRef.current?.scrollIntoView({ behavior: "smooth" })
+        if (type === Type.experience && experienceRef) experienceRef.current?.scrollIntoView({ behavior: "smooth" })
+        if (type === Type.work && workRef) workRef.current?.scrollIntoView({ behavior: "smooth" })
+        if (type === Type.contact && contactRef) contactRef.current?.scrollIntoView({ behavior: "smooth" })
+    }
+
     return (
-        <motion.div
-            variants={variants}
-            transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 0.6 }}
-            animate={hidden ? 'hidden' : 'visible'}
-            className={`fixed flex justify-between ${montserrat.className} items-center w-full py-2 px-4 h-fit backdrop-blur-sm`}>
-            <h1 className='text-4xl bold text-secondary'>
-                P
-            </h1>
-            <ul className='flex gap-2 items-center justify-end px-4'>
-                {list.map((l, key) =>
-                    <li key={key} className='px-2 text-secondary'>{l}</li>
-                )}
-            </ul>
-        </motion.div>
+        <>
+            <motion.div
+                variants={variants}
+                transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 0.6 }}
+                animate={hidden ? 'hidden' : 'visible'}
+                className={`fixed flex justify-between ${montserrat.className} items-center w-full py-2 px-4 h-fit backdrop-blur-sm`}>
+                <h1 className='text-4xl bold text-secondary'>
+                    P
+                </h1>
+                <ul className='flex gap-2 xs:hidden md:flex items-center justify-end px-4'>
+                    {list.map((l, key) =>
+                        <li key={key}
+                            onClick={() => handleNavClick(l.type)}
+                            className='cursor-pointer px-2 text-secondary capitalize'>{l.text}</li>
+                    )}
+                </ul>
+
+                <span onClick={() => setSideBarState(!sideBarState)}
+                    className='text-secondary md:hidden cursor-pointer'>
+                    <AiOutlineMenu size={25} />
+                </span>
+            </motion.div>
+
+            <motion.div
+                transition={{ duration: .8 }}
+                variants={sidebarVariants}
+                animate={sideBarState ? 'visible' : 'hidden'}
+                className='fixed right-0 w-2/4 flex justify-center items-center
+            min-h-screen bg-[#1f263a] z-50 shadow-2xl md:hidden'>
+                <span onClick={() => setSideBarState(false)}
+                    className='cursor-pointer absolute top-0 right-0 p-4 text-secondary'>
+                    <RxCross1 />
+                </span>
+                <ul className='flex flex-col gap-4 items-center 
+                    justify-between px-4'>
+                    {list.map((l, key) =>
+                        <li key={key}
+                            onClick={() => handleNavClick(l.type)}
+                            className='px-2 text-secondary capitalize cursor-pointer'>{l.text}</li>
+                    )}
+                </ul>
+            </motion.div>
+        </>
     )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import { Montserrat } from 'next/font/google'
 import Nav from '@/components/Nav'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Image from 'next/image'
+import { AiFillGithub, AiFillLinkedin, AiFillTwitterCircle, AiFillTwitterSquare } from 'react-icons/ai'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
@@ -22,9 +23,19 @@ export default function Home() {
     },
   })
 
+  const aboutRef = useRef<null | HTMLDivElement>(null);
+  const experienceRef = useRef<null | HTMLDivElement>(null);
+  const workRef = useRef<null | HTMLDivElement>(null);
+  const contactRef = useRef<null | HTMLDivElement>(null);
+
   return (
     <>
-      <Nav />
+      <Nav
+        aboutRef={aboutRef}
+        contactRef={contactRef}
+        experienceRef={experienceRef}
+        workRef={workRef}
+      />
       <div className={`xs:px-12 md:px-36 w-full h-full ${montserrat.className}`}>
         <section className='min-h-screen flex justify-start items-center w-full'>
           <div className='flex flex-col xs:gap-3 sm:gap-4 md:gap-6 justify-start items-start w-full leading-loose tracking-widest'>
@@ -35,105 +46,151 @@ export default function Home() {
         </section>
 
         {/* about me  */}
-        <section className='min-h-screen mb-4'>
-          <h1 className='text-2xl mb-11 font-bold underline text-secondary underline-offset-4 capitalize'>about me 😃</h1>
-          <div className='flex xs:flex-col md:flex-col xl:flex-row gap-4 justify-between'>
-            <div className='w-full tracking-wide'>
-              <p className='text-md'>Hi there 👋, I am Prasenjit Pawar, a software engineer passionate about building innovative and high-quality web applications 🚀. With expertise in frontend and backend development, I also bring a unique skill set to every project.
-                I have extensive experience working with various technologies, including React, Tailwind CSS, Node.js, and .NET Core. <br /><br />
-                I began my career as an <span className='text-secondary'>intern at a startup</span>, where I developed a deep understanding of the industry and honed my technical skills. I am a <span className='text-secondary'>software engineer at Capgemini</span>, where I continue to deliver exceptional results and contribute to the team&apos;s success.
-                <br /><br /> I will be a perfect choice if you are looking for a skilled and dedicated software engineer who can deliver exceptional results. Contact me today to discuss your project requirements and learn how I can help you achieve your goals.</p>
-            </div>
-            <div className='w-full md:hidden xl:block'>
-              {/* progile  */}
+        <section ref={aboutRef} className='min-h-screen mb-4'>
+          <div className='py-14'>
+            <h1 className='text-2xl mb-11 font-bold underline text-secondary underline-offset-4 capitalize'>about me 😃</h1>
+            <div className='flex xs:flex-col md:flex-col xl:flex-row w-full gap-12 justify-between'>
+              <div className='w-full tracking-wide'>
+                <p className='text-md'>Hi there 👋, I am Prasenjit Pawar, a software engineer passionate about building innovative and high-quality web applications 🚀. With expertise in frontend and backend development, I also bring a unique skill set to every project.
+                  I have extensive experience working with various technologies, including React, Tailwind CSS, Node.js, and .NET Core. <br /><br />
+                  I began my career as an <a className='text-secondary hover:underline cursor-pointer' target="_blank" rel="noopener noreferrer" href='https://www.banao.tech/'>intern at a startup</a>, where I developed a deep understanding of the industry and honed my technical skills.
+                  I am a <a className='text-secondary hover:underline cursor-pointer' target="_blank" rel="noopener noreferrer" href='https://www.capgemini.com/'>software engineer at Capgemini</a>, where I continue to deliver exceptional results and contribute to the team&apos;s success.
+                  <br /><br /> I will be a perfect choice if you are looking for a skilled and dedicated software engineer who can deliver exceptional results. Contact me today to discuss your project requirements and learn how I can help you achieve your goals.</p>
+              </div>
+              <div className='w-full xs:hidden xl:block'>
+                {/* progile  */}
+                <Image src={"/images/me.jpg"} alt="me" width={200} height={200}
+                  className={'opacity-50 shadow-2xl border-2 border-secondary p-2'} />
+              </div>
             </div>
           </div>
         </section>
 
-        {/* work experience  */}
-        <section className='min-h-screen flex flex-col items-center w-full'>
-          <h1 className='text-2xl mb-11 font-bold underline text-secondary underline-offset-4 capitalize'>Where I&apos;ve worked 💼</h1>
-          <div className='flex xs:flex-col md:flex-row gap-12'>
-            <ul className='flex  xs:flex-row md:flex-col'>
-              <li
-                onClick={() => setWorkData({
-                  ...workData,
-                  capgemini: { ...workData.capgemini, active: true },
-                  bano: { ...workData.bano, active: false },
-                })}
-                className={`p-2 xs:border-b-2 md:border-b-0 xs:border-l-0 md:border-l-2 transition duration-500 cursor-pointer 
-                  ${workData.capgemini.active ? 'border-secondary bg-[#2b313f] text-secondary' : 'border-[#7f8596]'} `}>
-                Capgemini
-              </li>
+        {/*  experience  */}
+        <section ref={experienceRef} className='min-h-screen w-full'>
+          <div className='flex flex-col items-center py-14'>
 
-              <li
-                onClick={() => setWorkData({
-                  ...workData,
-                  bano: { ...workData.bano, active: true },
-                  capgemini: { ...workData.capgemini, active: false },
-                })}
-                className={`p-2 xs:border-b-2 md:border-b-0 xs:border-l-0 md:border-l-2 transition duration-500 cursor-pointer
+            <h1 className='text-2xl mb-11 font-bold underline text-secondary underline-offset-4 capitalize'>Where I&apos;ve worked 💼</h1>
+            <div className='flex xs:flex-col md:flex-row gap-12'>
+              <ul className='flex  xs:flex-row md:flex-col'>
+                <li
+                  onClick={() => setWorkData({
+                    ...workData,
+                    capgemini: { ...workData.capgemini, active: true },
+                    bano: { ...workData.bano, active: false },
+                  })}
+                  className={`p-2 xs:border-b-2 md:border-b-0 xs:border-l-0 md:border-l-2 transition duration-500 cursor-pointer 
+                  ${workData.capgemini.active ? 'border-secondary bg-[#2b313f] text-secondary' : 'border-[#7f8596]'} `}>
+                  Capgemini
+                </li>
+
+                <li
+                  onClick={() => setWorkData({
+                    ...workData,
+                    bano: { ...workData.bano, active: true },
+                    capgemini: { ...workData.capgemini, active: false },
+                  })}
+                  className={`p-2 xs:border-b-2 md:border-b-0 xs:border-l-0 md:border-l-2 transition duration-500 cursor-pointer
                   ${workData.bano.active ? 'border-secondary bg-[#2b313f] text-secondary' : 'border-[#7f8596] '}`}>
-                Banao Technologies</li>
-            </ul>
-            <div>
-              {workData.capgemini.active &&
-                <div className='flex flex-col py-2 max-w-md'>
-                  <h1 className='capitalize text-ternary font-bold'> {workData.capgemini.role} <span className='text-secondary'>@ Capgemini</span></h1>
-                  <h1 className='text-sm'> {workData.capgemini.date} </h1>
-                  <ul>
-                    {workData.capgemini.achivements.map((l, i) =>
-                      <li className='list-disc py-2 text-secondary' key={i}><p>{l}</p></li>
-                    )}
-                  </ul>
-                </div>
-              }
-              {workData.bano.active &&
-                <div className='flex flex-col py-2 max-w-md'>
-                  <h1 className='capitalize text-ternary font-bold'> {workData.bano.role} <span className='text-secondary'>@ Banao Technologies</span></h1>
-                  <h1 className='text-sm'> {workData.bano.date} </h1>
-                  <ul>
-                    {workData.bano.achivements.map((l, i) =>
-                      <li className='list-disc py-2 text-secondary' key={i}><p>{l}</p></li>
-                    )}
-                  </ul>
-                </div>
-              }
+                  Banao Technologies</li>
+              </ul>
+              <div>
+                {workData.capgemini.active &&
+                  <div className='flex flex-col py-2 max-w-md'>
+                    <h1 className='capitalize text-ternary font-bold'> {workData.capgemini.role} <span className='text-secondary'>@ Capgemini</span></h1>
+                    <h1 className='text-sm'> {workData.capgemini.date} </h1>
+                    <ul>
+                      {workData.capgemini.achivements.map((l, i) =>
+                        <li className='list-disc py-2 text-secondary' key={i}><p>{l}</p></li>
+                      )}
+                    </ul>
+                  </div>
+                }
+                {workData.bano.active &&
+                  <div className='flex flex-col py-2 max-w-md'>
+                    <h1 className='capitalize text-ternary font-bold'> {workData.bano.role} <span className='text-secondary'>@ Banao Technologies</span></h1>
+                    <h1 className='text-sm'> {workData.bano.date} </h1>
+                    <ul>
+                      {workData.bano.achivements.map((l, i) =>
+                        <li className='list-disc py-2 text-secondary' key={i}><p>{l}</p></li>
+                      )}
+                    </ul>
+                  </div>
+                }
+              </div>
             </div>
           </div>
         </section>
 
 
         {/* projects  */}
-        <section className='min-h-full mb-4'>
-          <h1 className='text-2xl mb-11 font-bold underline text-secondary underline-offset-4 capitalize'>Things I&apos;ve build ⚙️</h1>
-          <div className='flex flex-col'>
-            {/* prj 1 */}
-            <a target="_blank" rel="noopener noreferrer" href='https://resume-builder-frontend-ten.vercel.app/'
-              className='relative mb-32 transition duration-700'>
-              <div className='relative bg-[#1c2234] w-fit rounded p-2 cursor-pointer shadow hover:shadow-2xl'>
-                <h1 className='text-ternary text-2xl font-bold mb-3'>Resume builder </h1>
-                <p className='text-primary'>A simple tool for building resume <br />
-                  <span className='capitalize text-secondary'>tech stack used:</span> React.js, Express, Tailwind CSS, Prisma, MongoDB
-                </p>
-                <Image alt='resumebuilder' className='absolute w-96 xs:hidden xl:block -right-72 -top-2 opacity-50 -z-10' src='/images/resumebuilder.png' />
-              </div>
-            </a>
+        <section ref={workRef} className='min-h-full mb-4'>
+          <div className='py-14'>
+            <h1 className='text-2xl mb-11 font-bold underline text-secondary underline-offset-4 capitalize'>Things I&apos;ve build ⚙️</h1>
+            <div className='flex flex-col'>
+              {/* prj 1 */}
+              <a target="_blank" rel="noopener noreferrer" href='https://resume-builder-frontend-ten.vercel.app/'
+                className='relative mb-32 transition duration-700'>
+                <div className='relative bg-[#1c2234] w-fit rounded p-2 cursor-pointer shadow hover:shadow-2xl'>
+                  <h1 className='text-ternary text-2xl font-bold mb-3'>Resume builder </h1>
+                  <p className='text-primary'>A simple tool for building resume <br />
+                    <span className='capitalize text-secondary'>tech stack used:</span> React.js, Express, Tailwind CSS, Prisma, MongoDB
+                  </p>
+                  <Image alt='resumebuilder' width={200} height={200}
+                    className='absolute w-96 xs:hidden xl:block -right-72 -top-2 opacity-50 -z-10'
+                    src='/images/resumebuilder.png' />
+                </div>
+              </a>
+            </div>
           </div>
         </section>
 
 
         {/* contact  */}
 
-        <section className='flex flex-col gap-4 justify-center min-h-screen items-center'>
-          <h1 className='text-md text-secondary  capitalize'>
-            wha&tapos;s next?
-          </h1>
-          <h1 className='font-bold capitalize text-ternary text-3xl'>get in touch</h1>
-          <a className='p-3 text-secondary border-secondary border-2 transition duration-400
-          hover:opacity-50'
-            // hover:bg-ternary hover:border-primary hover:text-primary'
-            href={`mailto: prasenjitpawar22@gmail.com}`}> Say Hello</a>
+        <section ref={contactRef} className=''>
+          <div className='py-12 flex flex-col min-h-screen justify-between items-center'>
+            <div className='flex flex-col flex-grow gap-5 justify-center items-center'>
+              <div className='flex flex-col justify-center items-center'>
+                <h1 className='text-md text-secondary  capitalize'>
+                  what&apos;s next?
+                </h1>
+                <h1 className='font-bold capitalize text-ternary text-3xl'>get in touch</h1>
+              </div>
+              <a className='p-3 text-secondary border-secondary border-2 transition duration-400
+                hover:text-ternary hover:border-ternary'
+                target="_blank" rel="noopener noreferrer" href={`mailto: prasenjitpawar22@gmail.com}`}> Say Hello</a>
+              <p className='max-w-lg text-center'>Please don&apos;t hesitate to reach out to me with any opportunities or simply to say hello.
+                My inbox is always open and I&apos;ll make sure to get back to you as soon as possible.
+                Whether you have a query or just want to connect, I look forward to hearing from you!</p>
+            </div>
+
+            <div className='flex flex-col gap-2'>
+              <ul className='flex items-center justify-center gap-2'>
+                <li className='hover:text-secondary hover:cursor-pointer'>
+                  <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/prasenjit-pawar-2b378b77/">
+                    <AiFillLinkedin size={30} />
+                  </a>
+                </li>
+                <li className='hover:text-secondary hover:cursor-pointer'>
+                  <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/PrasenjitPawar">
+                    <AiFillTwitterCircle size={30} />
+                  </a>
+                </li>
+                <li className='hover:text-secondary hover:cursor-pointer'>
+                  <a target="_blank" rel="noopener noreferrer" href="https://github.com/prasenjitpawar22">
+                    <AiFillGithub size={30} />
+                  </a>
+                </li>
+              </ul>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/prasenjitpawar22/profile-web" className='text-ternary text-sm '>
+                Design copyed from{' '}
+                <a className='text-secondary transition duration-300 hover:underline '
+                  target="_blank" rel="noopener noreferrer" href="https://github.com/bchiang7">@Brittany Chiang</a>
+                {' &'} Build By Prasenjit Pawar
+              </a>
+            </div>
+          </div>
         </section>
 
       </div >
