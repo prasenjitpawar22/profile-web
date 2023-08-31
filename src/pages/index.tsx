0e17986b02c47df37c9bf1b1182d6e5022eff5a7
Prasenjit Pawar <prasenjitpawar22@gmail.com>
2023-08-31 16:14:14 +0530

    added prj cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { ArrowLeftIcon, ArrowRightIcon } from "lucide-react";
+
 import x from "../../public/twitter-x.svg";
 import insta from "../../public/instagram.svg";
 import linkedIn from "../../public/linkedin.svg";
@@ -8,6 +10,33 @@ import git from "../../public/github.svg";
 import hashnode from "../../public/hashnode.svg";
 
 export default function Home() {
+  const projects = [
+    {
+      link: "https://link3-pi.vercel.app/",
+      img: "/link.png",
+      title: "Link3",
+      description: "link-a-bio tool",
+    },
+    {
+      link: "https://resume-builder-frontend-ten.vercel.app/",
+      img: "/resumeBuilder.png",
+      title: "ResumeBuilder",
+      description: "reume build tool",
+    },
+    {
+      link: "https://bonk-a-alien.vercel.app/",
+      img: "/bonk.png",
+      title: "Bonk-a-Alien",
+      description: "A simple game",
+    },
+    {
+      link: "https://weather-app-kappa-seven-88.vercel.app/",
+      img: "/weather.png",
+      title: "Weather-App",
+      description: "A weather app",
+    },
+  ];
+
   return (
     <div className="flex h-screen flex-col overflow-y-scroll xs:px-12 xs:pt-12 lg:px-80 lg:pt-14">
       <div className="mb-12">
@@ -103,46 +132,39 @@ export default function Home() {
       </div>
       <div className="mb-12 flex flex-col">
         <p className="mb-3 font-bold text-slate-400">Projects</p>
-        <ul className="items-center text-slate-100">
-          <li>
-            <Link
-              className="hover:underline"
-              target={"_blank"}
-              href={"https://link3-pi.vercel.app/"}
-            >
-              Link3 - <span className="font-thin">link-a-bio tool</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              target={"_blank"}
-              className="hover:underline"
-              href={"https://resume-builder-frontend-ten.vercel.app/"}
-            >
-              ResumeBuilder -{" "}
-              <span className="font-thin">reume build tool</span>
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              target={"_blank"}
-              className="hover:underline"
-              href={"https://bonk-a-alien.vercel.app/"}
-            >
-              Bonk-a-Alien - <span className="font-thin">A simple game</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              target={"_blank"}
-              className="hover:underline"
-              href={"https://weather-app-kappa-seven-88.vercel.app/"}
-            >
-              Weather-app - <span className="font-thin">A weather app</span>
-            </Link>
-          </li>
-        </ul>
+        <div className="relative flex gap-x-2">
+          <div
+            id="sliderContainer"
+            className="w-full rounded border border-slate-700"
+          >
+            <ul id="slider" className="flex w-full overflow-auto">
+              {projects.map((project, index) => (
+                <li key={index} className="card w-96 p-5">
+                  <Link
+                    target={"_blank"}
+                    href={project.link}
+                    className="flex w-36 select-none flex-col items-center rounded p-2 text-slate-100 shadow-sm 
+                    shadow-slate-500 transition duration-200 hover:bg-slate-800 hover:shadow-slate-800"
+                  >
+                    <div className="relative mb-2 h-24 w-full object-cover opacity-60">
+                      <Image
+                        src={project.img}
+                        layout="fill"
+                        objectFit="cover"
+                        className="rounded"
+                        alt={project.title}
+                      />
+                    </div>
+                    <div className="w-full text-center">
+                      <h2 className="font-bold ">{project.title}</h2>
+                      <p className="font-thin">{project.description}</p>
+                    </div>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
       <div>
         <p className="font-bold text-slate-400">
