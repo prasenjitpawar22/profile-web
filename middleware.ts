959e3c27167c581ea46582a2089484b731aac0f6
Prasenjit Pawar <prasenjitpawar22@gmail.com>
2024-02-22 13:06:53 +0530

    test need to remove latter

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+import { NextFetchEvent, NextRequest } from "next/server";
+
+export async function middleware(req:NextRequest) {
+  let domain = req.headers.get("host") as string;
+
+  console.log(domain);
+  
+  return NextResponse.redirect("http://localhost:3000", );
+    
+}
