4ccdf12044b51eda75959fd6668a7e4bf37dddcb
Prasenjit Pawar <prasenjitpawar22@gmail.com>
2024-06-16 23:36:45 +0530

    portfolio update

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
-import PageMainBlockHolder from "@/components/page-main-block-holder";
+import GitHubIcon from "@/components/github-icon";
+import LinkedInIcon from "@/components/linkedIn-icon";
+import XIcon from "@/components/x-icon";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-background">
-      <div className="flex flex-col items-center justify-center">
-        <div className="xl:w-[500px] md:w-[300px] w-full py-4">
-          <PageMainBlockHolder />
+    <main className="flex min-h-screen flex-col p-24">
+      <div className="flex flex-col gap-4">
+        <Image
+          src={"/avatar.png"}
+          height={80}
+          width={80}
+          alt="Prasenjit Pawar"
+          className="rounded-full border"
+        />
+        <h1 className="text-4xl font-semibold tracking-tight">
+          Software Engineer. Writer.
+        </h1>
+        <p className="text-base text-stone-700 tracking-wide ">
+          Hi, I am Prasenjit Pawar.
+        </p>
+        <div className="flex gap-3 items-center">
+          <XIcon /> <GitHubIcon /> <LinkedInIcon />
         </div>
       </div>
-    </div>
+    </main>
   );
 }
