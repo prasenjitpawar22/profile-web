d1659d11c7163bddd40c90555bf91eaa432ced92
prasenjit pawar <prasenjitpawar22@gmail.com>
2024-10-08 21:43:45 +0530

    glide radio button added

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -2,10 +2,11 @@ import { CardsShuffle } from '@/components/weekend-components/cards-shuffle'
 import DiscoButton from '@/components/weekend-components/disco-button'
 import { DynamicActionBar } from '@/components/weekend-components/dynamic-action-bar/index'
 import { SliderThrow } from '@/components/weekend-components/dynamic-action-bar/slider-throw'
+import { GlideRadioButton } from '@/components/weekend-components/glide-radio-button'
 import InlinePopup from '@/components/weekend-components/inline-popup'
 import { NormalAccordion } from '@/components/weekend-components/normal-accordion'
 import SlideUpButton from '@/components/weekend-components/slide-up-button'
-import Test from '@/components/weekend-components/testing/test'
+// import { Test } from '@/components/weekend-components/testing/test'
 import Timer from '@/components/weekend-components/timer'
 
 export default function Page() {
@@ -31,6 +32,7 @@ export default function Page() {
         <NormalAccordion />
         <Timer />
         {/* <Test /> */}
+        <GlideRadioButton />
       </div>
     </>
   )
--- components/weekend-components/testing/test.tsx
+++ components/weekend-components/testing/test.tsx
@@ -1,29 +1,57 @@
 'use client'
 
-import { useEffect, useState } from 'react'
-import { MotionValue, motion, useSpring, useTransform } from 'framer-motion'
+import { motion, Transition, Variants, } from 'framer-motion'
+import { useState } from 'react';
+import { getCoreRowModel, useReactTable } from '@tanstack/react-table'
 
-export default function Test() {
-  const [counter, setCounter] = useState(0)
-  const animatedY = useSpring(counter)
+const radios = [
+  {
+    id: 1,
+    name: 'Creator',
+    value: 'Creator',
+  },
+  {
+    id: 2,
+    name: 'Bidder',
+    value: 'Bidder',
+  },
+]
 
-  useEffect(() => {
-    animatedY.set(counter)
-  }, [animatedY, counter])
+export function Test() {
+  const [selected, setSelected] = useState<string | null>('Creator')
 
   return (
-    <div className='bg-slate-50 w-[500px] relative h-[200px] items-center justify-center flex'>
-      <motion.span
-        style={{ y: animatedY }}
-        className='text-black inset-0 absolute w-fit'>
-        text
-      </motion.span>
-      <input
-        type='number'
-        className=''
-        step={50}
-        onChange={(e) => setCounter(parseInt(e.target.value))}
-      />
+    <div className='bg-slate-50 w-full relative h-[200px] gap-4 items-center flex-col justify-center flex rounded-md shadow border'>
+      <div className='flex items-center justify-center gap-4'>
+        {radios.map((radio) => (
+          <div key={radio.id} className='flex items-center gap-2 relative'>
+            <input type="radio" name="radio" id={radio.value} value={radio.value} className='hidden' />
+            <motion.div
+              onClick={() => setSelected(radio.value)}
+              className='w-4 h-4 bg-white border border-black rounded-full relative cursor-pointer'>
+              {selected === radio.value &&
+                <motion.div layoutId="radio-circle"
+                  initial={false}
+                  animate={{
+                    scale: [1, 2, 2, 1],
+                    rotate: [10, 120, 120, 10],
+                    borderRadius: ['50%', '20%', '50%', '50%'],
+                  }}
+                  className='absolute left-0 bottom-0 m-auto top-0 right-0 w-[60%] h-[60%] bg-black rounded-full'></motion.div>
+              }
+            </motion.div>
+            <label
+              onClick={() => setSelected(radio.value)}
+              htmlFor={radio.value}>{radio.name}</label>
+          </div>
+        ))}
+      </div>
+      <div className='flex items-center flex-col justify-center gap-1'>
+        <p className='text-sm'>{selected}</p>
+        <p>A simple radio button made with simple animations.</p>
+      </div>
     </div>
   )
 }
+
+
