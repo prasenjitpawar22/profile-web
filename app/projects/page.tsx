ca2c74ad430e8ef66c2af79b8f66466bbf4d0dec
Prasenjit Pawar <prasenjitpawar22@gmail.com>
2025-02-26 21:41:03 +0530

    down

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,7 +5,7 @@ import { ReactNode } from "react";
 type Status = 'Completed' | 'Incomplete' | 'Inprogress' | 'abandoned'|'down'
 
 const projectList: { link: string, name: string, description: string, status: Status, logo: ReactNode, techStack: string }[] = [{
-  name: 'Vendue', link: 'https://vendue-web.vercel.app/', status: 'Completed', description: 'A realtime bidding app', logo: <svg width="50" height="50" viewBox="0 0 151 151" fill="none" xmlns="http://www.w3.org/2000/svg">
+  name: 'Vendue', link: 'https://vendue-web.vercel.app/', status: 'down', description: 'A realtime bidding app', logo: <svg width="50" height="50" viewBox="0 0 151 151" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M0 75.5C0 51.7334 0 39.8501 3.95773 30.5036C9.02245 18.5429 18.5429 9.02245 30.5036 3.95773C39.8501 0 51.7334 0 75.5 0V0C99.2666 0 111.15 0 120.496 3.95773C132.457 9.02245 141.978 18.5429 147.042 30.5036C151 39.8501 151 51.7334 151 75.5V75.5C151 99.2666 151 111.15 147.042 120.496C141.978 132.457 132.457 141.978 120.496 147.042C111.15 151 99.2666 151 75.5 151V151C51.7334 151 39.8501 151 30.5036 147.042C18.5429 141.978 9.02245 132.457 3.95773 120.496C0 111.15 0 99.2666 0 75.5V75.5Z" fill="url(#paint0_linear_44_41)" />
     <path d="M109.504 48.064L81.472 118H66.784L38.752 48.064H51.808C53.216 48.064 54.352 48.4 55.216 49.072C56.08 49.712 56.736 50.544 57.184 51.568L70.72 89.008C71.36 90.64 71.984 92.432 72.592 94.384C73.2 96.304 73.776 98.32 74.32 100.432C74.768 98.32 75.264 96.304 75.808 94.384C76.384 92.432 76.992 90.64 77.632 89.008L91.072 51.568C91.424 50.672 92.048 49.872 92.944 49.168C93.872 48.432 95.008 48.064 96.352 48.064H109.504Z" fill="url(#paint1_linear_44_41)" />
     <defs>
